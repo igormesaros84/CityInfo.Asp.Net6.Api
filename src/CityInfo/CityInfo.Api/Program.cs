dfd981b682b276d7c4b74c56b39d23b3da55c753
Igor Meszaros <igor.mesaros@bighand.com>
2023-08-10 22:34:32 +0100

    Added some comments to programs.cs

--- src/CityInfo/CityInfo.Api/Program.cs
+++ src/CityInfo/CityInfo.Api/Program.cs
@@ -1,8 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// Built in dependency injection
 
+// Register services that are required for API's
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
