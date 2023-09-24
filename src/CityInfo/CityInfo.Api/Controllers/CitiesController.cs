9a48f8827db6804474852b709ffdddd2d67a2a0b
Igor Meszaros <igor.mesaros@bighand.com>
2023-09-24 21:34:16 +0100

    Setup Routing

--- src/CityInfo/CityInfo.Api/Controllers/CitiesController.cs
+++ src/CityInfo/CityInfo.Api/Controllers/CitiesController.cs
@@ -5,6 +5,7 @@ namespace CityInfo.Api.Controllers;
 [ApiController]
 public class CitiesController : ControllerBase
 {
+    [HttpGet("api/cities")]
     public JsonResult GetCities()
     {
         return new JsonResult(
