b707682e52174d33f98783bc64f84406f0156a21
Igor Meszaros <igor.mesaros@bighand.com>
2023-09-24 22:23:06 +0100

    Return correct status code

--- src/CityInfo/CityInfo.Api/Controllers/CitiesController.cs
+++ src/CityInfo/CityInfo.Api/Controllers/CitiesController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CityInfo.Api.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CityInfo.Api.Controllers;
 
@@ -7,10 +8,22 @@ namespace CityInfo.Api.Controllers;
 public class CitiesController : ControllerBase
 {
     [HttpGet]
-    public JsonResult GetCities()
+    public ActionResult<CityDto[]> GetCities()
     {
-        return new JsonResult(
-                CitiesDataStore.Instance.Cities
-            );
+        return Ok(CitiesDataStore.Instance.Cities);
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<CityDto> GetCity(int id)
+    {
+        var city = CitiesDataStore.Instance.Cities.Find(c => c.Id == id);
+
+        if (city == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(city);
+            
     }
 }
