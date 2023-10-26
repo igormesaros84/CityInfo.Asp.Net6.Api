4699b108fa1f9add4fd2bd6292b9bb83df60fb22
Igor Meszaros <igor.mesaros@bighand.com>
2023-10-26 21:51:06 +0100

    Seed data to the db

--- src/CityInfo/CityInfo.Api/DbContexts/CityInfoContext.cs
+++ src/CityInfo/CityInfo.Api/DbContexts/CityInfoContext.cs
@@ -13,6 +13,47 @@ public class CityInfoContext : DbContext
     {
         
     }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<City>().HasData(
+            new City("New York City")
+            { 
+                Id = 1, 
+                Description = "The one with that big park." 
+            },
+            new City("Antwerp")
+            {
+                Id = 2,
+                Description = "That weird german band or IDK."
+            },
+            new City("Paris")
+            {
+                Id = 3,
+                Description = "The one with that big tower."
+            });
+
+        modelBuilder.Entity<PointOfInterest>().HasData(
+            new PointOfInterest("Central Park")
+            {
+                Id = 1,
+                CityId = 1,
+                Description = "Big Park"
+            },
+            new PointOfInterest("Unfinished cathedral")
+            {
+                Id = 2,
+                CityId = 2,
+                Description = "Unfinished Cathedral"
+            },
+            new PointOfInterest("Eiffel Tower")
+            {
+                Id = 3,
+                CityId = 3,
+                Description = "Big tower"
+            });
+        base.OnModelCreating(modelBuilder);
+    }
     // This is on way of configuring the database to the context. But we went with a different approach that can be seen in Program.cs
 
     //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
