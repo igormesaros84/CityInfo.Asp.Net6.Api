08131782c3f2a76ae0dbc5d6a7d7a03f5fc302c8
Igor Meszaros <igor.mesaros@bighand.com>
2023-12-21 10:49:33 +0000

    Add Authorization to Cities controller

--- src/CityInfo/CityInfo.Api/Controllers/CitiesController.cs
+++ src/CityInfo/CityInfo.Api/Controllers/CitiesController.cs
@@ -1,12 +1,14 @@
 ﻿using AutoMapper;
 using CityInfo.Api.Models;
 using CityInfo.Api.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Text.Json;
 
 namespace CityInfo.Api.Controllers;
 
 [ApiController]
+[Authorize]
 [Route("api/cities")]
 public class CitiesController : ControllerBase
 {
